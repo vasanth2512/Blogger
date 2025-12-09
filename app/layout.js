5d99b0349d1aaaa7a83355f8a57ebc21f0414c55
vasanth2512 <jayavelvasanth@outlook.com>
2025-12-09 13:52:40 +0530

    updated bg color

--- app/layout.js
+++ app/layout.js
@@ -17,8 +17,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={outfit.className}>
+      <body 
+        className={`${outfit.className} bg-white `}>
         {children}
       </body>
     </html>
